3db825b08d519c9eaaa87b7be8eb3f8863d8ab65
Rob <robertoscandale1@gmail.com>
2025-06-16 23:51:00 +0200

    aggiunta metodi per inserire ed eliminare liste di libri

--- src/main/storage/ArchivioLibri.java
+++ src/main/storage/ArchivioLibri.java
@@ -9,7 +9,11 @@ public interface ArchivioLibri {
 
     public void inserisciLibro(Libro l);
 
+    public void inserisciLibro(List<Libro> libri);
+
     public void eliminaLibro(String ISBN);
+
+    public void eliminaLibro(List<String> ListaISBN);
 
     public List<Libro> visualizzaLibri();
 
