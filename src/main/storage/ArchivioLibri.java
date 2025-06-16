6027577d5c56571a186eb19d9f4a397a55d8ae76
Rob <robertoscandale1@gmail.com>
2025-06-16 23:54:15 +0200

    correzione nomi funzioni

--- src/main/storage/ArchivioLibri.java
+++ src/main/storage/ArchivioLibri.java
@@ -7,15 +7,15 @@ import java.util.List;
 public interface ArchivioLibri {
 
 
-    public void inserisciLibro(Libro l);
+    public void inserisci(Libro l);
 
-    public void inserisciLibro(List<Libro> libri);
+    public void inserisci(List<Libro> libri);
 
-    public void eliminaLibro(String ISBN);
+    public void elimina(String ISBN);
 
-    public void eliminaLibro(List<String> ListaISBN);
+    public void elimina(List<String> ListaISBN);
 
-    public List<Libro> visualizzaLibri();
+    public List<Libro> visualizza();
 
     //da agiungere visualizzazione con fintri e ordinamento
 
