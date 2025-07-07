ef2417ff3a242414f2b2d336e312dc900bd2b975
Rob <robertoscandale1@gmail.com>
2025-07-07 19:11:02 +0200

    fix algoritmo per il filtraggio per titolo

--- src/main/java/query/filtro/FiltroPerTitolo.java
+++ src/main/java/query/filtro/FiltroPerTitolo.java
@@ -15,8 +15,30 @@ public class FiltroPerTitolo extends AbstractFiltroArchivio<String>{
         if(this.parametroFiltro.equalsIgnoreCase(l.getTitolo()))
             return  true;
 
-        String[] sottoStringheTitolo = this.parametroFiltro.split(" ");
+//        String[] sottoStringheTitoloInserito = this.parametroFiltro.split(" ");
+//        String[] sottoStringheTitolo = l.getTitolo().split(" ");
+//
+//        if(sottoStringheTitolo.length - sottoStringheTitoloInserito.length  <= 1)
+//            return false;
+//
+//
+//
+//        return Arrays.stream(sottoStringheTitoloInserito).anyMatch(l.getTitolo()::contains);
 
-        return Arrays.stream(sottoStringheTitolo).anyMatch(l.getTitolo()::contains);
+
+        String[] sottoStringheTitoloInput = this.parametroFiltro.split("\\s+");
+        String[] sottoStringheTitolo = l.getTitolo().split("\\s+");
+
+        // Ignora ricerche troppo brevi (es. 1 sola parola da 2 lettere)
+        if (sottoStringheTitoloInput.length == 1 && sottoStringheTitoloInput[0].length() <= 2)
+            return false;
+
+        long paroleTrovate = Arrays.stream(sottoStringheTitoloInput)
+                .filter(p -> Arrays.asList(sottoStringheTitolo).contains(p))
+                .count();
+
+        // Almeno il 60% delle parole devono essere presenti
+        double percentualeMatch = (double) paroleTrovate / sottoStringheTitoloInput.length;
+        return percentualeMatch >= 0.6;
     }
 }
